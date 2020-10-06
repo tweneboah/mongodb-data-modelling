806811326f8b65c45da7e28a6d03b926525adafd
tweneboah <twentekghana@gmail.com>
2020-10-06 08:38:08 +0800

    structured

---
+++ config/mongodbConnect.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const dbConnect = async () => {
+  try {
+    mongoose.connect(
+      'mongodb://localhost/users',
+      {
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+      },
+      () => {
+        console.log('DB connected');
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = dbConnect;
